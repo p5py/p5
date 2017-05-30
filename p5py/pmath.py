880f67452a6452066a9d03c33990ea0c546b4f70
Abhik Pal <theabhikpal@gmail.com>
2017-05-31 00:42:21 +0530

    DOCS: Change doc format to Sphinx default.
    We were using NumPy's docstring format before; reverted that back to
    sphinx defaults.

--- p5py/pmath.py
+++ p5py/pmath.py
@@ -34,92 +34,79 @@ class Vector:
     two or three dimensional space is a geometric entity that has some
     magnitude (or length) and a direction.
 
-    Parameters
-    ----------
-    x, y : int or float
-        The x and y components of the vector
-    z : int or float, optional
-        The z-component of the vector. Only required for three
-        dimensional vectors. This is zero by default.
+    Examples::
 
+        >>> vec_2d = Vector(3, 4)
+        >>> vec_2d
+        Vector(3.00, 4.00, 0.00)
 
-    Attributes
-    ----------
-    x, y, z : int or float
-        The x, y, z components of the vector.
-    angle
-    magnitude
-    sq_magnitude
+        >>> vec_3d = Vector(2, 3, 4)
+        >>> vec_3d
+        Vector(2.00, 3.00, 4.00)
 
+    :param x: The x-component of the vector.
+    :param y: The y-component of the vector.
+    :param z: The z-component of the vector (0 by default; only
+        required for 3D vectors; )
 
-    Examples
-    --------
+    :type x: int or float
+    :type y: int or float
+    :type z: int or float
 
-    >>> vec_2d = Vector(3, 4)
-    >>> vec_2d
-    Vector(3.00, 4.00, 0.00)
-
-    >>> vec_3d = Vector(2, 3, 4)
-    >>> vec_3d
-    Vector(2.00, 3.00, 4.00)
     """
 
     def __init__(self, x, y, z=0):
+        #: The x-component of the vector.
         self.x = x
+
+        #: The y-component of the vector.
         self.y = y
+
+        #: The z-component of the vector.
+        #:
+        #: This attribute is only required for three dimensional
+        #: vectors. Defaults to 0.
         self.z = z
 
     @property
     def angle(self):
-        """float: the angle of rotation of the vector (in radians).
+        """The angle of rotation of the vector (in radians).
 
-        Returns
-        -------
-        float
-            The angle of rotation of the vector.
+        This attribute isn't available for three dimensional vectors.
 
-        Raises
-        ------
-        AssertionError
-            When the vector is three dimensional.
 
-        Notes
-        -----
-            Talking about the angle of a vector only makes sense when
-            the vector is two dimensional. Hence, raise an
-            AssertionError when the vector is three dimensional.
+        Examples::
 
-        Examples
-        --------
-        >>> from math import pi, isclose
+            >>> from math import pi, isclose
+            >>> p = Vector(1, 0, 0)
+            >>> isclose(p.angle, 0)
+            True
 
-        >>> p = Vector(1, 0, 0)
-        >>> isclose(p.angle, 0)
-        True
+            >>> p = Vector(0, 1, 0)
+            >>> isclose(p.angle, pi/2)
+            True
 
-        >>> p = Vector(0, 1, 0)
-        >>> isclose(p.angle, pi/2)
-        True
+            >>> p = Vector(1, 1, 1)
+            >>> p.angle
+            Traceback (most recent call last):
+                ...
+            AssertionError: Can't compute the angle for a 3D vector.
 
-        >>> p = Vector(1, 1, 1)
-        >>> p.angle
-        Traceback (most recent call last):
-            ...
-        AssertionError: Can't compute the angle for a 3D vector.
+            >>> p = Vector(1, 1)
+            >>> isclose(p.angle, pi/4)
+            True
+            >>> p.angle = pi/2
+            >>> isclose(p.angle, pi/2)
+            True
 
-        >>> p = Vector(1, 1)
-        >>> isclose(p.angle, pi/4)
-        True
-        >>> p.angle = pi/2
-        >>> isclose(p.angle, pi/2)
-        True
+            >>> p = Vector(1, 1)
+            >>> isclose(p.angle, pi/4)
+            True
+            >>> p.rotate(pi/4)
+            >>> isclose(p.angle, pi/2)
+            True
 
-        >>> p = Vector(1, 1)
-        >>> isclose(p.angle, pi/4)
-        True
-        >>> p.rotate(pi/4)
-        >>> isclose(p.angle, pi/2)
-        True
+        :raises AssertionError: If the vector is three-dimensional
 
         """
         # TODO (abhikpal, 2017-05-25):
@@ -133,11 +120,16 @@ class Vector:
         self.rotate(theta -  self.angle)
 
     def rotate(self, theta):
-        """Rotate the vector by an angle."""
+        """Rotates the vector by an angle.
+        
+        :param theta: Angle (in radians).
+        :type theta: float or int
+
+        """
         self.x = self.x * math.cos(theta) - self.y * math.sin(theta)
         self.y = self.x * math.sin(theta) + self.y * math.cos(theta)
 
-    def angle_between(self, other, amount):
+    def angle_between(self, other):
         """Calculate the angle between two vectors."""
         # TODO (abhikpal, 2017-05-25):
         #     Use a @ b == abs(a) * abs(b) * cos(theta) to get the req. angle.
@@ -145,25 +137,24 @@ class Vector:
 
     @property
     def magnitude(self):
-        """float: the magnitude of the vector.
+        """The magnitude of the vector.
 
-        Recompute the components of the vector when the magnitude is
-        changed.
+        Examples::
 
-        >>> p = Vector(2, 3, 6)
-        >>> p.magnitude
-        7.0
+            >>> p = Vector(2, 3, 6)
+            >>> p.magnitude
+            7.0
 
-        >>> abs(p)
-        7.0
+            >>> abs(p)
+            7.0
 
-        >>> p.magnitude = 14
-        >>> p
-        Vector(4.0, 6.0, 12.0)
+            >>> p.magnitude = 14
+            >>> p
+            Vector(4.0, 6.0, 12.0)
 
-        >>> p.normalize()
-        >>> print(p)
-        Vector(0.29, 0.43, 0.86)
+            >>> p.normalize()
+            >>> print(p)
+            Vector(0.29, 0.43, 0.86)
 
         """
         return math.sqrt(self.dot(self))
@@ -177,7 +168,7 @@ class Vector:
 
     @property
     def magnitude_sq(self):
-        """float: the squared magnitude of the vector."""
+        """The squared magnitude of the vector."""
         return self.dot(self)
 
     @magnitude_sq.setter
@@ -195,16 +186,11 @@ class Vector:
     def __iter__(self):
         """Return the components of the vector as an iterator.
 
-        Yields
-        ------
-        x, y, z
-            The components of the vector.
+        Examples::
 
-        Examples
-        --------
-        >>> p = Vector(2, 3, 4)
-        >>> print([ c for c in p])
-        [2, 3, 4]
+            >>> p = Vector(2, 3, 4)
+            >>> print([ c for c in p])
+            [2, 3, 4]
 
         """
         yield self.x
@@ -214,92 +200,76 @@ class Vector:
     def __add__(self, other):
         """Add two vectors.
 
-        Parameters
-        ----------
-        other : Vector
+        Examples::
 
-        Returns
-        -------
-        Vector
-            The vector obtained by adding the corresponding components
-            of the two vectors
+            >>> p = Vector(2, 3, 6)
+            >>> q = Vector(3, 4, 5)
+            >>> p + q
+            Vector(5.00, 7.00, 11.00)
 
-        Examples
-        --------
-        >>> p = Vector(2, 3, 6)
-        >>> q = Vector(3, 4, 5)
-        >>> p + q
-        Vector(5.00, 7.00, 11.00)
+        :param other:
+        :type other: Vector
+        :returns: The vector obtained by adding the corresponding
+            components of the two vectors.
+        :rtype: Vector
+
         """
         return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def __sub__(self, other):
         """Subtract one vector from another.
 
-        Parameters
-        ----------
-        other : Vector
+        Examples::
 
-        Returns
-        -------
-        Vector
-            The vector obtained by subtracting the corresponding
-            components of the `other` vector from those of `self`
+            >>> p = Vector(2, 3, 6)
+            >>> q = Vector(3, 4, 5)
+            >>> p - q
+            Vector(-1.00, -1.00, 1.00)
 
-        Examples
-        --------
-        >>> p = Vector(2, 3, 6)
-        >>> q = Vector(3, 4, 5)
-        >>> p - q
-        Vector(-1.00, -1.00, 1.00)
+        :param other:
+        :type other: Vector
+        :returns: The vector obtained by subtracteing  the corresponding
+            components of the vector from those of another.
+        :rtype: Vector
+
         """
         return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
 
-    def __mul__(self, other):
+    def __mul__(self, k):
         """Multiply the vector by a scalar.
 
+        Examples::
 
-        Parameters
-        ----------
-        other : int, float
+            >>> p = Vector(2, 3, 6)
+            >>> p * 2
+            Vector(4.00, 6.00, 12.00)
 
-        Returns
-        -------
-        Vector
-            The vector obtained by multiplying each component of
-            `self` by other.
+            >>> 2 * p
+            Vector(4.00, 6.00, 12.00)
 
-        Raises
-        ------
-        TypeError
-            When `other` is non-numeric.
+            >>> p * p
+            Traceback (most recent call last):
+                ...
+            TypeError: Can't multiply/divide a vector by a non-numeric.
 
-        Examples
-        --------
+            >>> p = Vector(2, 3, 6)
+            >>> -p
+            Vector(-2.00, -3.00, -6.00)
 
-        >>> p = Vector(2, 3, 6)
-        >>> p * 2
-        Vector(4.00, 6.00, 12.00)
+            >>> p = Vector(2, 3, 6)
+            >>> p / 2
+            Vector(1.00, 1.50, 3.00)
 
-        >>> 2 * p
-        Vector(4.00, 6.00, 12.00)
-
-        >>> p * p
-        Traceback (most recent call last):
-            ...
-        TypeError: Can't multiply/divide a vector by a non-numeric.
-
-        >>> p = Vector(2, 3, 6)
-        >>> -p
-        Vector(-2.00, -3.00, -6.00)
-
-        >>> p = Vector(2, 3, 6)
-        >>> p / 2
-        Vector(1.00, 1.50, 3.00)
+        :param k:
+        :type k: int, float
+        :returns: The vector obtained by multiplying each component of
+            `self` by k.
+        :rtype: Vector
+        :raises TypeError: When `k` is non-numeric.
 
         """
-        if isinstance(other, int) or isinstance(other, float):
-            return Vector(self.x * other, self.y * other, self.z * other)
+        if isinstance(k, int) or isinstance(k, float):
+            return Vector(self.x * k, self.y * k, self.z * k)
         raise TypeError("Can't multiply/divide a vector by a non-numeric.")
 
     def __rmul__(self, other):
@@ -316,23 +286,19 @@ class Vector:
     def cross(self, other):
         """Return the cross product of the two vectors.
 
-        Parameters
-        ----------
-        other : Vector
+        Examples::
 
-        Returns
-        -------
-        Vector
-            The vector perpendicular to both `self` and `other` i.e.,
-            the vector obtained by taking the cross product of `self`
-            and `other`.
+            >>> i = Vector(1, 0, 0)
+            >>> j = Vector(0, 1, 0)
+            >>> i.cross(j)
+            Vector(0.00, 0.00, 1.00)
 
-        Examples
-        --------
-        >>> i = Vector(1, 0, 0)
-        >>> j = Vector(0, 1, 0)
-        >>> i.cross(j)
-        Vector(0.00, 0.00, 1.00)
+        :param other:
+        :type other: Vector
+        :returns: The vector perpendicular to both `self` and `other`
+            i.e., the vector obtained by taking the cross product of
+            `self` and `other`.
+        :rtype: Vector
 
         """
         return Vector(self.y * other.z - self.z * other.y,
@@ -342,23 +308,20 @@ class Vector:
     def dot(self, other):
         """Compute the dot product of two vectors.
 
-        Parameters
-        ----------
-        other : Vector
+        Examples::
 
-        Returns
-        -------
-        int or float
-            The dot product of the two vectors.
+            >>> p = Vector(2, 3, 6)
+            >>> q = Vector(3, 4, 5)
+            >>> p.dot(q)
+            48
+            >>> p @ q
+            48
 
-        Examples
-        --------
-        >>> p = Vector(2, 3, 6)
-        >>> q = Vector(3, 4, 5)
-        >>> p.dot(q)
-        48
-        >>> p @ q
-        48
+        :param other:
+        :type other: Vector
+        :returns: The dot product of the two vectors.
+        :rtype: int or float
+
         """
         return sum(sc*oc for sc, oc in zip(self, other))
 
