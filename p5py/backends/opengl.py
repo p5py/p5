ae4272f8c7fed4050e371cbb2cbeb4529ea55101
Abhik Pal <theabhikpal@gmail.com>
2017-05-31 23:45:15 +0530

    Add Shader class for handling OpenGL shaders

--- p5py/backends/opengl.py
+++ p5py/backends/opengl.py
@@ -20,6 +20,129 @@ from ctypes import *
 
 from pyglet.gl import *
 
+
+class Shader:
+    """Represents a shader in OpenGL.
+
+    :param source: GLSL source code/filename for the shader.
+    :type source: str or bytes
+
+    :param kind: the type of the shader (should ideally be a const
+        from pyglet.gl)
+    :type kind: int
+
+    :param pid: The ID of the program to which the shader belongs.
+        (Optional; defaults to None)
+    :type pid: int
+
+    :param from_file: Set to True if `source` specifies a filename;
+        False otherwise.
+    :type filename: bool
+
+    """
+
+    def __init__(self, source, kind, program=None, from_file=False):
+        if from_file:
+            with open(self.filename) as f:
+                src = f.read()
+            self.source = src
+        else:
+            self.source = source
+
+        self.__pid = program
+        self.__kind = kind
+        self.__sid = None
+
+    def compile(self):
+        """Generate a shader id and compile the shader"""
+        self.__sid = glCreateShader(self.__kind)
+        src = c_char_p(self.source)
+        glShaderSource(
+            self.sid,
+            1,
+            cast(pointer(src), POINTER(POINTER(c_char))),
+            None
+        )
+        glCompileShader(self.sid)
+
+    def attach(self, pid=None):
+        """Attach the shader to the given program.
+
+        :param pid: The program id.
+        :type pid: int
+
+        """
+        if self.pid and (not pid):
+            pid = self.pid
+
+        glAttachShader(pid, self.sid)
+
+    @property
+    def source(self):
+        """Return the GLSL source code for the shader.
+
+        :rtype: str
+        """
+        return self.__source
+
+    @source.setter
+    def source(self, src):
+        if isinstance(src, bytes):
+            self.__source = src
+        else:
+            self.__source = src.encode('utf-8')
+
+    @property
+    def kind(self):
+        """Return the type of the shader.
+
+        :rtype: int
+        :raises AttributeError: On modification attempt.
+
+        """
+        return self.__kind
+
+    @kind.setter
+    def kind(self, k):
+        raise AttributeError("Cannot modify the shader type"
+                             "after it has been instantiated")
+
+    @property
+    def sid(self):
+        """Return the shader id of the shader.
+
+        :rtype: int
+        :raises AttributeError: On modification attempt.
+        :raises NameError: If the shader hasn't been created.
+
+        """
+        if self.__sid:
+            return self.__sid
+        else:
+            raise NameError("Shader hasn't been created yet.")
+
+    @sid.setter
+    def sid(self, k):
+        raise AttributeError("The shader id is read-only.")
+
+    @property
+    def pid(self):
+        """Returns the id of the program to which the shader is attached.
+
+        :rtype: int
+        :raises AttributeError: On modification attempt.
+        :raises NameError: If the shader hasn't been attached to a program.
+
+        """
+        if self.__pid:
+            return self.__pid
+        raise NameError("Shader hasn't been attached to a program yet.")
+
+    @pid.setter
+    def pid(self, k):
+        raise AttributeError("The program id is read-only.")
+
+
 vertex_shader_source = b"""
     #version 130
     
