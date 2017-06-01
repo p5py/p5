8b91a0abd02276803cd10c9c1734a386581a8b48
Abhik Pal <theabhikpal@gmail.com>
2017-06-01 13:07:48 +0530

    Add edge support for Shape; Add line()

--- p5py/primitives.py
+++ p5py/primitives.py
@@ -20,14 +20,26 @@ from .sketch import _p5_artist
 from .sketch import _p5_attribute
 from .sketch import _p5_transformation
 
-# we will pass around these shape objects internally to draw and
-# render stuff. A Shape will contain attributes for changing its shape
-# type -- POLY, LINE, etc -- and its shape attributes -- color,
-# stroke, etc --.
 class Shape:
-    def __init__(self, kind, *vertices):
+    """Represents a Shape in p5py.
+
+    :param kind: The type of this shape. Should be one of {'POLY', ...}
+    :type kind: str
+
+    :param vertices: A list of vertices (3-tuples) that make up the
+        shape.
+    :type vertices: list of tuples
+
+    :param edges: A list of indices into the vertices list that
+        represent edges. (Defaults to the empty list `[]`)
+    :type edges: list of tuples
+
+    """
+
+    def __init__(self, kind, vertices, edges=[]):
         self.kind = kind
         self.vertices = vertices
+        self.edges = edges
 
     def __repr__(self):
         return "({} Shape with vertices {})".format(self.kind, self.vertices)
@@ -36,12 +48,42 @@ class Shape:
 
 @_p5_artist
 def point(x, y, z=0):
-    """Returns a point Shape"""
-    return Shape('POLY', (x, y, z))
+    """Returns a point Shape.
+
+    :param x: x-coordinate of the shape.
+    :type x: int or float
+
+    :param y: y-coordinate of the shape.
+    :type y: int or float
+
+    :param z: z-coordinate of the shape (defaults to 0).
+    :type z: int or float
+
+    :returns: A point Shape.
+    :rtype: Shape
+
+    """
+    return Shape('POLY', [(x, y, z)])
 
 @_p5_artist
-def line(*args):
-    """Returns a line Shape"""
+def line(start_point, end_point):
+    """Returns a line Shape.
+
+    :param start_point: Coordinates of the starting point of the line.
+    :type start_point: tuple
+
+    :param end_point: Coordinates of the end point of the line.
+    :type end_point: tuple
+
+    :returns: A line Shape.
+    :rtype: Shape
+
+    """
+    if len(start_point) == 2:
+        start_point = *start_point, 0
+    if len(end_point) == 2:
+        end_point = *end_point, 0
+    return Shape('POLY', [start_point, end_point], [(0, 1)])
 
 @_p5_artist
 def rect(*args):
