981e0d7684de990c701e341d02409fc2d6a964d4
Abhik Pal <theabhikpal@gmail.com>
2017-06-20 03:22:56 +0530

    add more info to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from distutils.core import setup
 setup(
     name='p5',
     version='0.1.0dev1',
+    description="p5 is a python package based on the core ideas of Processing.",
+    license="GPLv3",
+    author="Abhik Pal",
     author_email='theabhikpal@gmail.com',
     url='https://p5py.github.io',
     packages=[
-        'p5', 'p5.core', 'p5.opengl', 'p5.pmath', 'p5.sketch',
-        'p5.tmp',
-    ]
+        'p5', 'p5.core','p5.opengl', 'p5.pmath', 'p5.sketch', 'p5.tmp'
+    ],
 )
