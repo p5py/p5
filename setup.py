ece87452342349a844eae6adf6b4472c03678589
Abhik Pal <theabhikpal@gmail.com>
2017-07-06 02:19:24 +0530

    update for dev3 release

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from os import path
 
 setup(
     name='p5',
-    version='0.1.0dev2',
+    version='0.1.0dev3',
     license="GPLv3",
     description="p5 is a python package based on the core ideas of Processing.",
     author="Abhik Pal",
     author_email='theabhikpal@gmail.com',
     url='https://p5py.github.io',
-    packages=find_packages(exclude=['tmp', 'tests', 'tools']),
+    packages=find_packages(exclude=['tools']),
     install_requires=['pyglet'],
 )
