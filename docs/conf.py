08b7c26408b197c473986c532b477a161d3af1e8
Abhik Pal <theabhikpal@gmail.com>
2017-07-14 14:21:43 +0530

    DOCS: RTFD-FIX add pyglet mocks

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,15 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
 
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['pyglet']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 sys.path.insert(0, os.path.abspath('../..'))
 
 import p5
