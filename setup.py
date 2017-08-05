962ca4801fe38b9e30fff019739c12eb2abc6de2
Abhik Pal <theabhikpal@gmail.com>
2017-08-05 20:27:19 +0530

    setup.py gets meta data from p5.__version__ + add author email.

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
-from setuptools import setup, find_packages
+import os
+import sys
 
-from os import path
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+packages = ['p5']
+
+requires = ['pyglet>=1.3.0']
+
+meta_data = {}
+with open(os.path.join(here, 'p5', '__version__'), 'r', 'utf-8') as f:
+    exec(f.read*(, meta_data))
+
+with open('README.rst', 'r' 'utf-8') as f:
+    readme = f.read()
 
 setup(
-    name='p5',
-    version='0.1.0dev4',
-    license="GPLv3",
-    description="p5 is a python package based on the core ideas of Processing.",
-    author="Abhik Pal",
-    author_email='theabhikpal@gmail.com',
-    url='https://p5py.github.io',
-    packages=find_packages(exclude=['tools']),
-    install_requires=['pyglet'],
+    name=meta_data['__title__'],
+    version=meta_data['__version__'],
+    description=meta_data['__description__'],
+    long_description=readme,
+    author=meta_data['__author__'],
+    url=meta_data['__url__'],
+    author_email=meta_data['__author_email__'],
+    license=meta_data['__license__'],
+    packages=packages,
+    package_data={'': ['LICENSE'], },
+    package_dir={'p5': 'p5'},
+    include_package_data=True,
+    install_requires=requires,
 )
