0c3d35781e465e8f3363e805dc9ec62b921b03d0
Abhik Pal <theabhikpal@gmail.com>
2017-08-16 20:23:23 +0530

    Color components are stored in their normalized forms.

--- p5/core/color.py
+++ p5/core/color.py
@@ -20,6 +20,7 @@ import colorsys
 import math
 
 from ..pmath import lerp
+from ..pmath import constrain
 
 __all__ = ['color_mode', 'Color']
 
@@ -37,19 +38,7 @@ def color_mode(mode):
     global color_parse_mode
     color_parse_mode = mode
 
-def to_rgb(h, s, b):
-    red, green, blue = colorsys.hsv_to_rgb(h/255, s/255, b/255)
-    red *= 255
-    green *= 255
-    blue *= 255
-    return red, green, blue
 
-def to_hsb(r, g, b):
-    hue, saturation, brightness = colorsys.rgb_to_hsv(r/255, g/255, b/255)
-    hue *= 255
-    saturation *= 255
-    brightness *= 255
-    return hue, saturation, brightness
 
 def parse_color(*args, color_mode='RGB', **kwargs):
     """Parses a color from a range of different input formats.
@@ -93,45 +82,65 @@ def parse_color(*args, color_mode='RGB', **kwargs):
     else:
         alpha = 255
 
+    hsb = None
+    rgb = None
+
     if len(args) == 1:
         gray = args[0]
-        red, green, blue = gray, gray, gray
+        rgb = gray, gray, gray
     elif len(args) == 2:
         gray, alpha = args
-        red, green, blue =  gray, gray, gray
+        rgb =  gray, gray, gray
     elif (len(args) == 3) and color_mode.startswith('RGB'):
-        red, green, blue = args
+        rgb = args
     elif (len(args) == 3) and color_mode.startswith('HSB'):
-        hue, saturation, brightness = args
-        red, green, blue = to_rgb(hue, saturation, brightness)
+        hsb = args
     elif (len(args) == 4) and color_mode.startswith('RGB'):
-        red, green, blue, alpha = args
+        _r, _g, _b, alpha = args
+        rgb = (_r, _g, _b)
     elif (len(args) == 4) and color_mode.startswith('HSB'):
-        hue, saturation, value, alpha = args
-        red, green, blue = to_rgb(hue, saturation, value)
+        _h, _s, _b, alpha = args
+        hsb = (_h, _s. _b)
     elif 'gray' in kwargs:
         gray = kwargs['gray']
-        red, green, blue = gray, gray, gray
+        rgb = gray, gray, gray
     elif all(param in kwargs for param in ['red', 'green', 'blue']):
-        red = kwargs['red']
-        green = kwargs['green']
-        blue = kwargs['blue']
+        _r = kwargs['red']
+        _g = kwargs['green']
+        _b = kwargs['blue']
+        rgb = (_r, _g, _b)
     elif all(param in kwargs for param in ['r', 'g', 'b']):
-        red = kwargs['r']
-        green = kwargs['g']
-        blue = kwargs['b']
+        _r = kwargs['r']
+        _g = kwargs['g']
+        _b = kwargs['b']
+        rgb = (_r, _g, _b)
     elif all(param in kwargs for param in ['hue', 'saturation', 'brightness']):
-        hue = kwargs['hue']
-        saturation = kwargs['saturation']
-        brightness = kwargs['brightness']
-        red, green, blue = to_rgb(hue, saturation, brightness)
+        _h = kwargs['hue']
+        _s = kwargs['saturation']
+        _b = kwargs['brightness']
+        hsb = (_h, _s, _b)
     elif all(param in kwargs for param in ['h', 's', 'b']):
-        hue = kwargs['h']
-        saturation = kwargs['s']
-        brightness = kwargs['b']
-        red, green, blue = to_rgb(hue, saturation, brightness)
+        _h = kwargs['h']
+        _s = kwargs['s']
+        _b = kwargs['b']
+        hsb = (_h, _s, _b)
     else:
         raise ValueError("Failed to parse color.")
+
+    if not (hsb is None):
+        h, s, b = hsb
+        h = constrain(h / color_range[0], 0, 1)
+        s = constrain(s / color_range[1], 0, 1)
+        b = constrain(b / color_range[2], 0, 1)
+        red, green, blue = colorsys.hsv_to_rgb(h, s, b)
+
+    if not (rgb is None):
+        r, g, b = rgb
+        red = constrain(r / color_range[0], 0, 1)
+        green = constrain(g / color_range[1], 0, 1)
+        blue = constrain(b / color_range[2], 0, 1)
+
+    alpha = constrain(alpha / color_range[3], 0, 1)
     return red, green, blue, alpha
 
 class Color:
@@ -141,37 +150,33 @@ class Color:
             color_mode = color_parse_mode
 
         if (len(args) == 1) and isinstance(args[0], Color):
-            r, g, b, a = args[0].rgba
+           r = args[0]._red
+           g = args[0]._green
+           b = args[0]._blue
+           a = args[0]._alpha
         else:
             r, g, b, a = parse_color(*args, color_mode=color_mode, **kwargs)
+
         self._red = r
         self._green = g
         self._blue = b
         self._alpha = a
-        self._normalized = (r/255, g/255, b/255, a/255)
 
         self._recompute_hsb()
 
-    def _recompute_norm(self):
-        """Recompute the normalized color from RGB values"""
-        self._normalized = (self._red / 255, self._green / 255,
-                            self._blue / 255, self._alpha / 255)
-
     def _recompute_rgb(self):
         """Recompute the RGB values from HSB values."""
-        r, g, b = to_rgb(self._hue, self._saturation, self._brightness)
+        r, g, b = colorsys.hsv_to_rgb(self._hue, self._saturation, self._brightness)
         self._red = r
         self._greeen = g
         self._blue = b
-        self._recompute_norm()
 
     def _recompute_hsb(self):
         """Recompute the HSB values from the RGB values."""
-        h, s, b = to_hsb(self._red, self._green, self._blue)
+        h, s, b = colorsys.rgb_to_hsv(self._red, self._green, self._blue)
         self._hue = h
         self._saturation = s
         self._brightness = b
-        self._recompute_norm()
 
     def lerp(self, target, amount):
         """Linearly interpolate one color to another by the given amount.
@@ -208,7 +213,7 @@ class Color:
     @property
     def normalized(self):
         """Normalized RGBA color values"""
-        return tuple(self._normalized)
+        return (self._red, self._green, self._blue, self._alpha)
 
     @property
     def gray(self):
@@ -236,10 +241,12 @@ class Color:
         #
         # - Conversion to grayscale, sample implementation (StackOverflow)
         # <https://stackoverflow.com/a/15686412>
-        return 0.299 * self._red + 0.587 * self._green + 0.144 * self._blue
+        norm_gray  = 0.299 * self._red + 0.587 * self._green + 0.144 * self._blue
+        return norm_gray * 255
 
     @gray.setter
     def gray(self, value):
+        value = constrain(value / 255, 0, 1)
         self._red = value
         self._green = value
         self._blue = value
@@ -248,12 +255,11 @@ class Color:
     @property
     def alpha(self):
         """The alpha value for the color."""
-        return self._alpha
+        return self._alpha * color_range[3]
 
     @alpha.setter
     def alpha(self, value):
-        self._alpha = value
-        self._recompute_norm()
+        self._alpha = constrain(value / color_range[3], 0, 1)
 
     @property
     def rgb(self):
@@ -261,7 +267,7 @@ class Color:
         :returns: Color components in RGB.
         :rtype: tuple
         """
-        return (self._red, self._green, self._blue)
+        return (self.red, self.green, self.blue)
 
     @property
     def rgba(self):
@@ -269,36 +275,37 @@ class Color:
         :returns: Color components in RGBA.
         :rtype: tuple
         """
-        return (self._red, self._green, self._blue, self._alpha)
+        return (self.red, self.green, self.blue, self.alpha)
+
 
     @property
     def red(self):
         """The red component of the color"""
-        return self._red
+        return self._red * color_range[0]
 
     @red.setter
     def red(self, value):
-        self._red = value
+        self._red = constrain(value / color_range[0], 0, 1)
         self._recompute_hsb()
 
     @property
     def green(self):
         """The green component of the color"""
-        return self._green
+        return self._green * color_range[1]
 
     @green.setter
     def green(self, value):
-        self._green = value
+        self._green = constrain(value / color_range[1], 0, 1)
         self._recompute_hsb()
 
     @property
     def blue(self):
         """The blue component of the color"""
-        return self._blue
+        return self._blue * color_range[2]
 
     @blue.setter
     def blue(self, value):
-        self._blue = value
+        self._blue = constrain(value / color_range[2], 0, 1)
         self._recompute_hsb()
 
     @property
@@ -315,36 +322,36 @@ class Color:
         :returns: Color components in HSBA.
         :rtype: tuple
         """
-        return (self._hue, self._saturation, self._brightness, self._alpha)
+        return (self.hue, self.saturation, self.brightness, self.alpha)
 
     @property
     def hue(self):
         """The hue component of the color"""
-        return self._hue
+        return self._hue * color_range[0]
 
     @hue.setter
     def hue(self, value):
-        self._hue = value
+        self._hue = constrain(value / color_range[0], 0, 1)
         self._recompute_rgb()
 
     @property
     def saturation(self):
         """The saturation component of the color"""
-        return self._saturation
+        return self._saturation * color_range[1]
 
     @saturation.setter
     def saturation(self, value):
-        self._saturation = value
+        self._saturation = constrain(value / color_range[1], 0, 1)
         self._recompute_rgb()
 
     @property
     def brightness(self):
         """The brightness component of the color"""
-        return self._brightness
+        return self._brightness * color_range[2]
 
     @brightness.setter
     def brightness(self, value):
-        self._brightness = value
+        self._brightness = constrain(value / color_range[2], 0, 1)
         self._recompute_rgb()
 
     # ...and some convenient aliases
