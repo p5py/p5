1c26c159324dfb64510f5810dd849b65c07d17ee
Abhik Pal <theabhikpal@gmail.com>
2017-08-21 03:11:58 +0530

    FIX: file name in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ packages = ['p5']
 requires = ['pyglet>=1.3.0']
 
 meta_data = {}
-with open(os.path.join(here, 'p5', '__version__'), 'r', 'utf-8') as f:
+with open(os.path.join(here, 'p5', '__version__.py'), 'r', 'utf-8') as f:
     exec(f.read(meta_data))
 
 with open('README.rst', 'r' 'utf-8') as f:
