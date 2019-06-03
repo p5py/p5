88ed36166f493c9f5fa67e3d9e3a6c2a3b9562f6
arihant parsoya <parsoyaarihant@gmail.com>
2019-06-03 19:50:37 +0530

    Created constants and p5.py environment file

---
+++ p5/core/constants.py
@@ -0,0 +1,39 @@
+#
+# Part of p5: A Python package based on Processing
+# Copyright (C) 2017-2018 Abhik Pal
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Constants Variables for P5 sketches"""
+
+# Environment modes
+P2D = "P2D"
+P3D = "P3D"
+
+# Stroke Cap Properties
+ROUND = "ROUND"
+SQUARE = "SQUARE"
+PROJECT = "PROJECT"
+
+# Stroke Join Properties
+MITER = "MITER"
+BEVEL = "BEVEL"
+ROUND = "ROUND"
+
+# Ellipse and Rectangle Modes
+RADIUS = "RADIUS"
+CENTER = "CENTER"
+CORNER = "CORNER"
+CORNERS = "CORNERS"
---
+++ p5/core/p5.py
@@ -0,0 +1,44 @@
+#
+# Part of p5: A Python package based on Processing
+# Copyright (C) 2017-2018 Abhik Pal
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Environment Variables for P5 sketches"""
+
+sketch = None
+renderer = None
+
+width = 360
+height = 360
+pixel_x_density = 1
+pixel_y_density = 1
+
+title = "p5"
+frame_count = -1
+frame_rate = None
+focused = True
+
+mouse_button = None
+mouse_is_pressed = False
+mouse_is_dragging = False
+mouse_x = 0
+mouse_y = 0
+pmouse_x = 0
+pmouse_y = 0
+key = None
+key_is_pressed = False
+
+pixels = None
