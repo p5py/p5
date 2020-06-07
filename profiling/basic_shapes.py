a7d0917f63915a7080cd0abd0aa6e14387dfbca6
Ziyao Zhang <ziyaointl@gmail.com>
2020-06-06 23:53:39 -0700

    Exit after drawing 100 frames

--- profiling/basic_shapes.py
+++ profiling/basic_shapes.py
@@ -1,30 +1,42 @@
 from p5 import *
 
+frames = 0
+FRAME_MAX = 100
+
+
 def setup():
-        # Sets the screen to be 720 pixels wide and 400 pixels high
-        size(720, 400)
+    # Sets the screen to be 720 pixels wide and 400 pixels high
+    size(720, 400)
+
 
 def draw():
-        background(0)
-        no_stroke()
+    global frames
+    if frames >= FRAME_MAX:
+        exit(0)
+    else:
+        frames += 1
 
-        fill(204)
-        triangle((18, 18), (18, 360), (81, 360))
+    background(0)
+    no_stroke()
 
-        fill(102)
-        rect((81, 81), 63, 63)
+    fill(204)
+    triangle((18, 18), (18, 360), (81, 360))
 
-        fill(204)
-        quad((189, 18), (216, 18), (216, 360), (144, 360))
+    fill(102)
+    rect((81, 81), 63, 63)
 
-        fill(255)
-        ellipse((252, 144), 72, 72)
+    fill(204)
+    quad((189, 18), (216, 18), (216, 360), (144, 360))
 
-        fill(204)
-        triangle((288, 18), (351, 360), (288, 360))
+    fill(255)
+    ellipse((252, 144), 72, 72)
 
-        fill(255)
-        arc((479, 300), 280, 280, PI, TWO_PI)
+    fill(204)
+    triangle((288, 18), (351, 360), (288, 360))
+
+    fill(255)
+    arc((479, 300), 280, 280, PI, TWO_PI)
+
 
 if __name__ == '__main__':
-        run()
+    run()
