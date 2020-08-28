1c55bd39e69906d25eaf7413c7a94f1d8f6639e6
nakul-shahdadpuri <nakulshahdadpuri3141@gmail.com>
2020-08-28 22:25:18 +0530

    get requst function done

--- p5/io/http.py
+++ p5/io/http.py
@@ -1,2 +1,15 @@
-def add(a,b):
-	return a+b
+from urllib import request
+import json
+
+
+
+
+def http_get(url):
+	try:
+		resp = request.urlopen(url)
+	except Exception:
+		print("Invalid url. Connection Failed.")
+		exit(1)
+	data = resp.read()
+	data = json.loads(data.decode("UTF-8"))
+	return data
