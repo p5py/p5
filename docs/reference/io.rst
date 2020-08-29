cbf8434c5ab97cb84daa3b8f690c560fbdb5566b
nakul-shahdadpuri <nakulshahdadpuri3141@gmail.com>
2020-08-29 18:43:36 +0530

    updated docs

---
+++ docs/reference/io.rst
@@ -0,0 +1,21 @@
+=====
+IO
+=====
+
+.. module:: p5
+   :noindex:
+
+HTTP
+=============
+
+http_get()
+-------
+
+.. autofunction:: http_get 
+
+
+http_post()
+------
+
+.. autofunction:: http_post
+
--- p5/io/http.py
+++ p5/io/http.py
@@ -4,6 +4,13 @@ import requests
 
 #synchronous
 def http_get(url):
+	"""
+	Performs a synchronous GET request on the given url.
+
+	:param url: url of the API
+	:type url: string
+
+	"""
 	try:
 		resp = request.urlopen(url)
 	except Exception:
@@ -15,6 +22,15 @@ def http_get(url):
 
 #synchronous
 def http_post(url,post_data):
+	"""
+	Performs a synchronous POST request on the given url.
+
+	:param url: url of the API
+	:type url: string
+	:param post_data: Key-value pairs of the request payload.
+	:type post_data: map 
+
+	"""
 	data = requests.post(url,post_data)
 	data = data.json()
 	return data
