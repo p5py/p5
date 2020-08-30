3f2cde3f9cdf7f50ad53272beccb1c08409f00d9
Ziyao Zhang <ziyaointl@gmail.com>
2020-08-29 17:08:10 -0700

    Add more sample scenes to profile

---
+++ profiling/spinning-cone.py
@@ -0,0 +1,24 @@
+from p5 import *
+
+def setup():
+    size(720, 400)
+
+def draw():
+    background(205, 102, 94)
+    rotate_x(frame_count * 0.02)
+    rotate_y(frame_count * 0.01)
+    blinn_phong_material()
+    # normal_material()
+    cone(200, 400)
+    # box(100, 100, 100)
+    ### Begin LIGHTS
+    # directional_light(1, 1, 1, 0, 0, -1)
+    locX = mouse_x - width/2
+    locY = mouse_y - height/2
+    light_specular(0, 0, 255)
+    point_light(360, 360*1.5, 360, locX, locY, 400)
+    ### End LIGHTS
+    # cone(200, 400)
+
+if __name__ == '__main__':
+    run(mode='P3D')
---
+++ profiling/tetrahedron.py
@@ -0,0 +1,32 @@
+from p5 import *
+
+def setup():
+    size(640, 360)
+    no_loop()
+
+def draw():
+    background(0)
+
+    stroke(255)
+    rotate_x(PI*3/2)
+    rotate_z(PI/6)
+    fill(255)
+    begin_shape()
+    vertex(-100, -100, -100)
+    vertex( 100, -100, -100)
+    vertex(   0,    0,  100)
+
+    vertex( 100, -100, -100)
+    vertex( 100,  100, -100)
+    vertex(   0,    0,  100)
+
+    vertex( 100, 100, -100)
+    vertex(-100, 100, -100)
+    vertex(   0,   0,  100)
+
+    vertex(-100,  100, -100)
+    vertex(-100, -100, -100)
+    vertex(   0,    0,  100)
+    end_shape()
+
+run(mode='P3D')
