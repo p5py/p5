a6a59adbf11c1d0980d00206dfdbb87c7f76eaae
Ziyao Zhang <ziyaointl@gmail.com>
2020-09-01 22:54:09 -0700

    Restrict python version to 3.6+

--- setup.py
+++ setup.py
@@ -60,10 +60,9 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
 )
