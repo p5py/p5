9a06f6480b928aedba0aff6e313eaf5cbc783001
Tushar <codingid6@gmail.com>
2021-08-16 23:49:51 +0530

    added some print statements for better logs

--- p5/visualTests/sanity_testing.py
+++ p5/visualTests/sanity_testing.py
@@ -13,12 +13,22 @@ test_names_3d = [f'{dirname}/sanityTests/3DSanityTests/{name}' for name in
                  os.listdir(os.path.join(dirname, 'sanityTests/3DSanityTests'))
                  if name not in ['__init__.py', '__pycache__']]
 
-test_names = test_names_2d + test_names_3d
-test_names.sort()
+print('============================= Running 2D tests =============================')
+for name in test_names_2d:
+    print('Running', name)
+    try:
+        # Run each test for 2s then raise a timeout
+        process = run(['python', name], stderr=PIPE, encoding='utf-8', timeout=2)
+    except subprocess.TimeoutExpired:
+        pass
+    else:
+        # If any error was found raise and exception
+        if process.stderr:
+            raise Exception(f'{process.stderr} in {name}')
 
-print(test_names)
 
-for name in test_names:
+print('============================= Running 3D tests =============================')
+for name in test_names_3d:
     print('Running', name)
     try:
         # Run each test for 2s then raise a timeout
