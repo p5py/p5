7e7acb34d415c0d43408baed32d6b4cdd6b0b305
Tushar <codingid6@gmail.com>
2021-08-16 23:28:59 +0530

    added 3D tests

--- p5/visualTests/sanity_testing.py
+++ p5/visualTests/sanity_testing.py
@@ -1,19 +1,31 @@
-import subprocess, os, sys
+import subprocess
+import os
 from subprocess import run, PIPE
-import sys
 
 e = SystemExit()
+
 dirname = os.path.dirname(__file__)
-test_names = [f'{dirname}/sanityTests/2DSanityTests/{name}' for name in
-              os.listdir(os.path.join(dirname, 'sanityTests/2DSanityTests'))
-              if name not in ['__init__.py', '__pycache__']]
+test_names_2d = [f'{dirname}/sanityTests/2DSanityTests/{name}' for name in
+                 os.listdir(os.path.join(dirname, 'sanityTests/2DSanityTests'))
+                 if name not in ['__init__.py', '__pycache__']]
+
+test_names_3d = [f'{dirname}/sanityTests/3DSanityTests/{name}' for name in
+                 os.listdir(os.path.join(dirname, 'sanityTests/3DSanityTests'))
+                 if name not in ['__init__.py', '__pycache__']]
+
+test_names = test_names_2d + test_names_3d
+test_names.sort()
+
+print(test_names)
 
 for name in test_names:
     print('Running', name)
     try:
+        # Run each test for 2s then raise a timeout
         process = run(['python', name], stderr=PIPE, encoding='utf-8', timeout=2)
     except subprocess.TimeoutExpired:
         pass
     else:
+        # If any error was found raise and exception
         if process.stderr:
             raise Exception(f'{process.stderr} in {name}')
