a0552798c3bca435cef081307680972a71cf8da8
tushar5526 <codingid6@gmail.com>
2022-09-07 16:32:52 +0530

    Bind all Processing APIs to Graphics object

--- p5/sketch/Skia2DRenderer/graphics.py
+++ p5/sketch/Skia2DRenderer/graphics.py
@@ -63,8 +63,109 @@ def bind(instance, func, as_name=None):
     return bound_method
 
 
-methods = [p5_lib.circle, p5_lib.background]
 
+methods = [
+    # Setting
+    p5_lib.background,
+    p5_lib.clear,
+    p5_lib.color_mode,
+    p5_lib.fill,
+    p5_lib.no_fill,
+    p5_lib.no_stroke,
+    p5_lib.stroke,
+
+    # Shape
+    p5_lib.arc,
+    p5_lib.ellipse,
+    p5_lib.circle,
+    p5_lib.point,
+    p5_lib.quad,
+    p5_lib.rect,
+    p5_lib.line,
+    p5_lib.square,
+    p5_lib.rect,
+
+    # Attributes
+    p5_lib.ellipse_mode,
+    p5_lib.rect_mode,
+    p5_lib.stroke_cap,
+    p5_lib.stroke_join,
+    p5_lib.stroke_weight,
+
+    # Curves
+    p5_lib.bezier,
+    p5_lib.bezier_detail,
+    p5_lib.bezier_point,
+    p5_lib.curve,
+    p5_lib.curve_detail,
+    p5_lib.curve_tightness,
+    p5_lib.curve_point,
+    p5_lib.curve_tangent,
+
+    # Vertex
+    p5_lib.begin_contour,
+    p5_lib.begin_shape,
+    p5_lib.bezier_vertex,
+    p5_lib.curve_vertex,
+    p5_lib.end_contour,
+    p5_lib.end_shape,
+    p5_lib.quadratic_vertex,
+    p5_lib.vertex,
+
+    # Structure
+    p5_lib.push,
+    p5_lib.pop,
+    p5_lib.push_matrix,
+    p5_lib.pop_matrix,
+    p5_lib.push_style,
+    p5_lib.pop_style,
+
+    # Transform
+    p5_lib.apply_matrix,
+    p5_lib.reset_matrix,
+    p5_lib.rotate,
+    p5_lib.scale,
+    p5_lib.shear_x,
+    p5_lib.shear_y,
+    p5_lib.translate,
+
+    # Local Storage
+    p5_lib.get_item,
+    p5_lib.clear_storage,
+    p5_lib.remove_item,
+    p5_lib.set_item,
+
+    # Image
+    p5_lib.save_canvas,
+    p5_lib.image,
+    p5_lib.tint,
+    p5_lib.no_tint,
+    p5_lib.image_mode,
+
+    p5_lib.load_pixels,
+    p5_lib.update_pixels,
+
+    p5_lib.noise,
+    p5_lib.noise_detail,
+    p5_lib.noise_seed,
+
+    # Random
+    p5_lib.random_seed,
+    p5_lib.random_uniform,
+    p5_lib.random_gaussian,
+
+    # Typography
+    p5_lib.text_align,
+    p5_lib.text_leading,
+    p5_lib.text_size,
+    p5_lib.text_style,
+    p5_lib.text_width,
+    p5_lib.text_ascent,
+    p5_lib.text_descent,
+    p5_lib.text_wrap,
+    p5_lib.text,
+    p5_lib.text_font,
+]
 
 def create_graphics_helper(width, height):
     graphics = SkiaGraphics(width, height)
