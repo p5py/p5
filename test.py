64edb5a3517ca4511e654dbfd9271925a839847b
tushar5526 <tushar5526@users.noreply.github.com>
2022-10-11 20:40:20 +0000

    :art: Python code fromated with psf/black

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from p5 import *
 
+
 def setup():
     size(300, 300)
+
 
 def draw():
     # print(width, height)
     circle(mouse_x, mouse_y, 20)
 
-run(renderer='skia')
+
+run(renderer="skia")
