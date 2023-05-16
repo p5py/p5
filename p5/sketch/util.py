9082b90786eefb59fc89e01ab4f2c57bc82f4ad5
AsukaMinato <asukaminato@nyan.eu.org>
2023-05-17 04:59:36 +0900

    add type to sketch (#431)

--- p5/sketch/util.py
+++ p5/sketch/util.py
@@ -1,19 +1,20 @@
 import pkgutil
 import os
+from typing import Tuple
 from ..core import p5
 import builtins
 
 
-def read_shader(filename):
+def read_shader(filename: str):
     """Reads a shader in string mode and returns the content"""
 
     return pkgutil.get_data("p5", os.path.join("sketch/", filename)).decode()
 
 
-def ensure_p3d(name):
-    assert p5.mode == "P3D", name + "is only available in P3D renderer"
+def ensure_p3d(name: str):
+    assert p5.mode == "P3D", f"{name}is only available in P3D renderer"
 
 
-def scale_tuple(t, scale=255):
+def scale_tuple(t: Tuple[float, ...], scale: float = 255):
     """Divides each element of tuple by scale"""
     return tuple(x / scale for x in t)
