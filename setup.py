a5974f4b4c17ee432e02e72c6155e42427c60cf8
Tushar <30565750+tushar5526@users.noreply.github.com>
2026-06-04 01:37:33 +0530

    chores: also add support for 3.12+ (#484)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     package_dir={"p5": "p5"},
     include_package_data=True,
     install_requires=requires,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -49,10 +49,11 @@ setup(
         "Topic :: Multimedia :: Graphics",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
